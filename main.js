801ff5f13334ba1b4f9ddb320af7658fc53bb62a
SnehalbSawant <69292647+SnehalbSawant@users.noreply.github.com>
2020-09-22 12:59:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var canvas = new fabric.Canvas('myCanvas');
+ 
+
+function new_image()
+{
+	fabric.Image.fromURL('BirthdayImage.jpg', function(Img) {
+	block_image_object = Img;
+		
+	block_image_object.scaleToWidth(700);
+	block_image_object.scaleToHeight(500);
+	block_image_object.set({
+	top:0,
+	left:50
+	});
+	canvas.add(block_image_object);
+	});
+	}
