3e8a489b6685b896c96e470a6577217530c0c0f8
SnehalbSawant <69292647+SnehalbSawant@users.noreply.github.com>
2021-05-05 14:04:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ function new_image()
 	block_image_object = Img;
 		
 	block_image_object.scaleToWidth(700);
-	block_image_object.scaleToHeight(500);
+	block_image_object.scaleToHeight(510);
 	block_image_object.set({
 	top:0,
-	left:10
+	left:0
 	});
 	canvas.add(block_image_object);
 	});
